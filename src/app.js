345b893af252b7fb7d08c9ef130de4a977ca01f5
Zhiyi Wu <privaterbok@gmail.com>
2016-10-04 01:44:20 -0700

    add fake db json and start to send getAll from frontend

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
 let Post = {
   findAll(){
     return new Promise((resolve, reject) => {
-      resolve('ok posts!');
+      let uri = "http://localhost:3000/posts"
+      // resolve('ok posts!');
+      let request = new XMLHttpRequest();
+      request.open("GET", uri, true); //true is async
+      request.onload = () => {
+        if(request.status >= 200 && request.status < 400) {
+          resolve( JSON.parse(request.response));
+        }
+      }
+
+      request.send();
     });
   }
 }
