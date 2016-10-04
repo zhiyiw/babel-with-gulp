91ee34a22b7b26e1bdaee263acdf0af66e77de84
Zhiyi Wu <privaterbok@gmail.com>
2016-10-04 02:03:37 -0700

    shows up the template

--- src/ui.js
+++ src/ui.js
@@ -1,7 +1,24 @@
 let ui = {
   renderPosts(posts){
-    console.log(posts);
+    // console.log(posts);
+    let elements = posts.map( (post)=> {
+      return articleElement;
+    });
+
+    let target = document.querySelector(".container");
+    target.innerHTML = elements.join(""); //fix a comma issue.
   }
 };
+
+let articleElement = 
+  `<article class='post'>
+    <h2 class='post-title'>
+     title!!
+    </h2>
+    <p class='post-meta'>
+      last reply on 
+    </p>
+  </article>`;
+
 
 export default ui;
