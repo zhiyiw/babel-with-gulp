9d99ae51618f5a4599ea496c8f044e9f50ed0959
Zhiyi Wu <privaterbok@gmail.com>
2016-10-04 01:25:30 -0700

    add small addition to Post and ui

--- src/app.js
+++ src/app.js
@@ -1,1 +1,14 @@
+let Post = {
+  findAll(){
+    return new Promise((resolve, reject) => {
+      resolve('ok posts!');
+    });
+  }
+}
+
+let ui = {
+  renderPosts(posts){
+    console.log(posts);
+  }
+}
 Post.findAll().then(ui.renderPosts);
